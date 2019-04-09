3f8fe1ce7c584ec106d439b2a40bf204ad9f2be3
Alfred Aho <dd2563@columbia.edu>
2019-04-09 03:12:02 -0400

    Templatize H-Tree.

--- histogram/HTree.hpp
+++ histogram/HTree.hpp
@@ -25,43 +25,54 @@ using std::make_shared;
 
 /*
  * An htree_node contains a vector of htree_elements. An htree_element contains
- * a key, which is an integer interval. Leaf elements will contain the entire
- * bucket, which has each dimension defined by the key of an ancestor (or the
- * leaf element itself). */
-class interval;
-class bucket;
-class htree_element;
-class htree_node;
+ * a key, which is an interval. Leaf elements will contain the entire bucket,
+ * which has each dimension defined by the key of an ancestor (or the leaf
+ * element itself). */
+template <typename T> class interval;
+template <typename T> class bucket;
+template <typename T> class htree_element;
+template <typename T> class htree_node;
 
+template <typename T>
 float overlap_proportion(
-    const interval &h_interval, const interval &query_interval);
-float overlap_proportion(const bucket &h_bkt, const bucket &query_bkt);
+    const interval<T> &h_interval, const interval<T> &query_interval);
+
+template <typename T>
+float overlap_proportion(const bucket<T> &h_bkt, const bucket<T> &query_bkt);
+
+template <typename T>
 float buckets_overlapped(
-    const vector< shared_ptr<bucket> > &h_bkts, const bucket &query_bkt);
+    const vector< shared_ptr< bucket<T> > > &h_bkts,
+    const bucket<T> &query_bkt);
 
-shared_ptr<htree_node> construct_htree(
-    vector< vector<int> > &tuples, vector<int> &num_buckets);
+template <typename T>
+shared_ptr< htree_node<T> > construct_htree(
+    vector< vector<T> > &tuples, vector<int> &num_buckets);
 
+template <typename T>
 void construct_hsubtree(
-    shared_ptr<htree_node> parent,
-    vector<interval> &current_bkt,
-    vector< vector<int> >::iterator tuples_begin,
-    vector< vector<int> >::iterator tuples_end,
+    shared_ptr< htree_node<T> > parent,
+    vector< interval<T> > &current_bkt,
+    typename vector< vector<T> >::iterator tuples_begin,
+    typename vector< vector<T> >::iterator tuples_end,
     vector<int> &num_buckets, int attr_index);
 
-int get_min_attr_value(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+T get_min_attr_value(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index);
 
-int get_min_attr_value(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+T get_max_attr_value(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index);
 
-interval get_attr_interval(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+interval<T> get_attr_interval(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index);
 
 
@@ -73,64 +84,70 @@ interval get_attr_interval(
  *  FIGLET: buckets
  */
 
-// An integer range. If it is unbounded from the left or right, has_min and
+// A range of T-values. If it is unbounded from the left or right, has_min and
 // has_max will be set to false respectively.
+template <typename T>
 class interval
 {
 public:
 
     bool has_min, has_max;
-    int min, max;
+    T min, max;
 
-    interval(int min, int max) :
+    interval(T min, T max) :
         has_min(true), has_max(true), min(min), max(max) {};
 
-    interval(bool has_min, int min, bool has_max, int max) :
+    interval(bool has_min, T min, bool has_max, T max) :
         has_min(has_min), has_max(has_max), min(min), max(max) {};
 
-    bool contains(int val) const {
+    bool contains(T val) const {
         return (!has_min || min <= val) && (!has_max || val <= max);
     }
 
 };
 
-std::ostream& operator<<(std::ostream &os, const interval i) {
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const interval<T> i) {
     os << "[" << i.min << ", " << i.max << "]";
     return os;
 }
 
-bool operator==(const interval a, const interval b) {
+template <typename T>
+bool operator==(const interval<T> a, const interval<T> b) {
     return ((!a.has_min && !b.has_min) || (a.min == b.min)) &&
         ((!a.has_max && !b.has_max) || (a.max == b.max));
 }
 
-bool operator!=(const interval a, const interval b) {
+template <typename T>
+bool operator!=(const interval<T> a, const interval<T> b) {
     return !(a == b);
 }
 
 
+template <typename T>
 class bucket {
 private:
-    vector<interval> dimensions;
+    vector< interval<T> > dimensions;
 
 public:
 
-    bucket(vector<interval> dimensions) : dimensions(dimensions) {}
+    bucket(vector< interval<T> > dimensions) : dimensions(dimensions) {}
 
-    interval operator[](const int index) const {
+    interval<T> operator[](const int index) const {
         return dimensions[index];
     }
-    
+
     int size() const {
         return dimensions.size();
     }
 
-    const vector<interval> &get_dimensions() const {
+    const vector< interval<T> > &get_dimensions() const {
         return dimensions;
     }
 };
 
-bool operator==(const bucket a, const bucket b) {
+template <typename T>
+bool operator==(const bucket<T> a, const bucket<T> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -142,23 +159,34 @@ bool operator==(const bucket a, const bucket b) {
     return true;
 }
 
-bool operator!=(const bucket a, const bucket b) {
+template <typename T>
+bool operator!=(const bucket<T> a, const bucket<T> b) {
     return !(a == b);
 }
 
+// For int values, the interval [x,y] contains y-x+1 points.
+int width(int x, int y) {
+    return y - x + 1;
+}
+
+double width(double x, double y) {
+    return y - x;
+}
+
 // Calculate the proportion |this union query_interval| / |this|.
+template <typename T>
 float overlap_proportion(
-    const interval &h_interval, const interval &query_interval)
+    const interval<T> &h_interval, const interval<T> &query_interval)
 {
     // h_interval should be an interval in an htree, which should not be
     // boundless in either direction (however, query_interval may be
     // unbounded).
     assert(h_interval.has_min && h_interval.has_max);
-    int left_bound = h_interval.min;
+    T left_bound = h_interval.min;
     if (query_interval.has_min) {
         left_bound = std::max(h_interval.min, query_interval.min);
     }
-    int right_bound = h_interval.max;
+    T right_bound = h_interval.max;
     if (query_interval.has_max) {
         right_bound = std::min(h_interval.max, query_interval.max);
     }
@@ -166,13 +194,13 @@ float overlap_proportion(
     if (right_bound - left_bound < 0) {
         return 0.0;
     }
-    // We assume int values, so the interval [x,y] contains y-x+1 points.
-    return (float) (right_bound - left_bound + 1) /
-        (h_interval.max - h_interval.min + 1);
+    return (float) width(left_bound, right_bound) /
+        width(h_interval.min, h_interval.max);
 }
 
 // Calculate the proportion |h_bkt union query_bkt| / |h_bkt|.
-float overlap_proportion(const bucket &h_bkt, const bucket &query_bkt) {
+template <typename T>
+float overlap_proportion(const bucket<T> &h_bkt, const bucket<T> &query_bkt) {
     float ratio = 1.0;
     for (int i = 0; i < h_bkt.size(); i++) {
         ratio *= overlap_proportion(h_bkt[i], query_bkt[i]);
@@ -183,8 +211,9 @@ float overlap_proportion(const bucket &h_bkt, const bucket &query_bkt) {
 // Used for selectivity/cardinality estimation. Estimate the number of buckets
 // (including partial buckets) that overlap with the query range, assuming a
 // uniform distribution within each bucket.
+template <typename T>
 float buckets_overlapped(
-    const vector< shared_ptr<bucket> > &h_bkts, const bucket &query_bkt)
+    const vector< shared_ptr< bucket<T> > > &h_bkts, const bucket<T> &query_bkt)
 {
     float sum = 0.0;
     for (int i = 0; i < h_bkts.size(); i++) {
@@ -202,28 +231,29 @@ float buckets_overlapped(
  *  FIGLET: nodes
  */
 
+template <typename T>
 class htree_element
 {
 private:
 
-    interval key;
+    interval<T> key;
     // Internal elements point to a child node (null for leaf elements).
-    shared_ptr<htree_node> child;
+    shared_ptr< htree_node<T> > child;
     // Leaf elements hold a bounding bucket defined by the keys of all elements
     // along the path from the root (null for internal elements).
-    shared_ptr<bucket> bkt;
+    shared_ptr< bucket<T> > bkt;
 
 public:
 
-    const interval& get_key() const {
+    const interval<T>& get_key() const {
         return key;
     }
 
-    const shared_ptr<htree_node>& get_child() const {
+    const shared_ptr< htree_node<T> >& get_child() const {
         return child;
     }
 
-    const shared_ptr<bucket>& get_bucket() const {
+    const shared_ptr< bucket<T> >& get_bucket() const {
         return bkt;
     }
 
@@ -231,28 +261,30 @@ public:
         return key.contains(val);
     }
 
-    // internal element
-    htree_element(interval key, shared_ptr<htree_node> child) :
+    // Internal element
+    htree_element(interval<T> key, shared_ptr< htree_node<T> > child) :
         key(key), child(child), bkt(nullptr) {};
 
-    // leaf element
-    htree_element(interval key, shared_ptr<bucket> bkt) :
+    // Leaf element
+    htree_element(interval<T> key, shared_ptr< bucket<T> > bkt) :
         key(key), child(nullptr), bkt(bkt) {};
 
     void print(std::ostream &os, int indent_level = 0) const;
 
 };
 
+template <typename T>
 class htree_node
 {
 private:
     // Leaf nodes are level 0; root is N-1, where N = number of attributes.
     unsigned int level;
-    vector<htree_element> elements;
+    vector< htree_element<T> > elements;
 
     // Note that there may be multiple elements with the same bound of width 1,
     // e.g. if all of the values in the column are equal.
-    vector<htree_element>::iterator find_min_overlapping(interval bounds) {
+    typename vector< htree_element<T> >::iterator
+    find_min_overlapping(interval<T> bounds) {
         assert(elements.size() > 0); // no node should be empty
         auto begin = elements.begin();
         int left_bound = bounds.min;
@@ -265,7 +297,8 @@ private:
         return begin;
     }
 
-    vector<htree_element>::iterator find_max_overlapping(interval bounds) {
+    typename vector< htree_element<T> >::iterator
+    find_max_overlapping(interval<T> bounds) {
         assert(elements.size() > 0); // no node should be empty
         auto end = elements.end() - 1;
         int right_bound = bounds.max;
@@ -280,13 +313,13 @@ private:
 
     // Recursive search helper
     void search_subhtree(
-        const bucket &query_range,
+        const bucket<T> &query_range,
         unsigned int attr_index,
-        vector< shared_ptr<bucket> > &results)
+        vector< shared_ptr< bucket<T> > > &results)
     {
         // attr_index increases as level decreases.
         assert(attr_index + level + 1 == query_range.size());
-        interval bounds = query_range[attr_index];
+        interval<T> bounds = query_range[attr_index];
 
         // Get "slice" of overlapping htree_elements.
         auto begin = find_min_overlapping(bounds);
@@ -319,31 +352,32 @@ public:
         return level;
     }
 
-    void insert(interval key, shared_ptr<htree_node> child) {
+    void insert(interval<T> key, shared_ptr< htree_node<T> > child) {
         elements.emplace_back(key, child);
     }
 
-    void insert(interval key, shared_ptr<bucket> bkt)
+    void insert(interval<T> key, shared_ptr< bucket<T> > bkt)
     {
         elements.emplace_back(key, bkt);
     }
 
     // Returns a list of pointers to buckets that overlap with the query range.
-    vector< shared_ptr<bucket> > search(const bucket &query_bkt) {
+    vector< shared_ptr< bucket<T> > > search(const bucket<T> &query_bkt) {
         assert(level == query_bkt.size() - 1); // must start search at root
-        vector< shared_ptr<bucket> > results;
+        vector< shared_ptr< bucket<T> > > results;
         search_subhtree(query_bkt, 0 /* attr_index */, results);
         return results;
     }
 
-    float estimateSelectivity(const bucket &query_bkt) {
+    float estimateSelectivity(const bucket<T> &query_bkt) {
         return buckets_overlapped(search(query_bkt), query_bkt);
     }
 
     void print(std::ostream &os, int indent_level = 0) const;
 };
 
-void htree_element::print(std::ostream &os, int indent_level) const {
+template <typename T>
+void htree_element<T>::print(std::ostream &os, int indent_level) const {
     std::string indent = "";
     for (int i = 0; i < indent_level; i++) {
         indent += "\t";
@@ -356,15 +390,16 @@ void htree_element::print(std::ostream &os, int indent_level) const {
     } else {
         // Leaf element
         os << indent << "{";
-        for (interval i : bkt->get_dimensions()) {
+        for (interval<T> i : bkt->get_dimensions()) {
             os << " " << i << ",";
         }
         os << " }\n";
     }
 }
 
-void htree_node::print(std::ostream &os, int indent_level) const {
-    for (htree_element elt : elements) {
+template <typename T>
+void htree_node<T>::print(std::ostream &os, int indent_level) const {
+    for (htree_element<T> elt : elements) {
         elt.print(os, indent_level);
     }
 }
@@ -384,18 +419,20 @@ void htree_node::print(std::ostream &os, int indent_level) const {
 
 // Given a vector of tuples and the number of buckets for each dimension,
 // construct an h-tree.
-shared_ptr<htree_node> construct_htree(
-    vector< vector<int> > &tuples, vector<int> &num_buckets)
+template <typename T>
+shared_ptr< htree_node<T> > construct_htree(
+    vector< vector<T> > &tuples, vector<int> &num_buckets)
 {
     if (tuples.size() == 0) {
         return nullptr;
     }
     // Initialize root node.
-    shared_ptr<htree_node> root = make_shared<htree_node>(tuples[0].size() - 1);
+    shared_ptr< htree_node<T> > root =
+        make_shared< htree_node<T> >(tuples[0].size() - 1);
 
     // Leaf nodes contain the keys of all ancestors, so we need to keep track
     // of the keys along the current path.
-    vector<interval> current_bkt;
+    vector< interval<T> > current_bkt;
 
     construct_hsubtree(
         root, current_bkt, tuples.begin(), tuples.end(), num_buckets, 0);
@@ -404,11 +441,12 @@ shared_ptr<htree_node> construct_htree(
 }
 
 // Recursive helper
+template <typename T>
 void construct_hsubtree(
-        shared_ptr<htree_node> node,
-        vector<interval> &current_bkt /* keys on current path */,
-        vector< vector<int> >::iterator tuples_begin,
-        vector< vector<int> >::iterator tuples_end,
+        shared_ptr< htree_node<T> > node,
+        vector< interval<T> > &current_bkt /* keys on current path */,
+        typename vector< vector<T> >::iterator tuples_begin,
+        typename vector< vector<T> >::iterator tuples_end,
         vector<int> &num_buckets /* number of buckets per dimension */,
         int attr_index)
 {
@@ -419,14 +457,14 @@ void construct_hsubtree(
 
     // Each loop adds an element to the given node. Each element represents a
     // partition of tuples with size <= bucket_capacity.
-    vector< vector<int> >::iterator partition_begin = tuples_begin;
+    typename vector< vector<T> >::iterator partition_begin = tuples_begin;
     while (partition_begin < tuples_end) {
-        vector< vector<int> >::iterator partition_end =
+        typename vector< vector<T> >::iterator partition_end =
             partition_begin + bucket_capacity;
         if (partition_end > tuples_end) {
             partition_end = tuples_end;
         }
-        interval key = get_attr_interval(
+        interval<T> key = get_attr_interval<T>(
             partition_begin, partition_end, attr_index);
 
         // Update current path.
@@ -434,7 +472,7 @@ void construct_hsubtree(
 
         if (node->is_internal()) {
             // Add internal element.
-            shared_ptr<htree_node> child = make_shared<htree_node>(
+            shared_ptr< htree_node<T> > child = make_shared< htree_node<T> >(
                 node->get_level() - 1);
             node->insert(key, child);
             // Construct subtree. The child nodes will further partition the
@@ -448,7 +486,7 @@ void construct_hsubtree(
                 attr_index + 1);
         } else {
             // Add leaf element.
-            shared_ptr<bucket> bkt = make_shared<bucket>(current_bkt);
+            shared_ptr< bucket<T> > bkt = make_shared< bucket<T> >(current_bkt);
             node->insert(key, bkt);
         }
 
@@ -460,9 +498,10 @@ void construct_hsubtree(
 }
 
 // Find the minimum value of the specified attribute.
-int get_min_attr_value(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+T get_min_attr_value(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index)
 {
     assert(begin < end);
@@ -474,9 +513,10 @@ int get_min_attr_value(
 }
 
 // Find the maximum value of the specified attribute.
-int get_max_attr_value(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+T get_max_attr_value(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index)
 {
     assert(begin < end);
@@ -488,13 +528,14 @@ int get_max_attr_value(
 }
 
 // Get the range of all values for the given attribute.
-interval get_attr_interval(
-    vector< vector<int> >::iterator begin,
-    vector< vector<int> >::iterator end,
+template <typename T>
+interval<T> get_attr_interval(
+    typename vector< vector<T> >::iterator begin,
+    typename vector< vector<T> >::iterator end,
     unsigned int attr_index)
 {
-    int key_min = get_min_attr_value(begin, end, attr_index);
-    int key_max = get_max_attr_value(begin, end, attr_index);
+    T key_min = get_min_attr_value<T>(begin, end, attr_index);
+    T key_max = get_max_attr_value<T>(begin, end, attr_index);
     return { key_min, key_max };
 }
 
