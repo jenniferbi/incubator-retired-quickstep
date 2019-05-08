9f3c0aec65c9b8739efbb02bc58b859b2d99a55c
Jennifer Bi <jb3495@columbia.edu>
2019-05-08 15:51:57 -0400

    full scripts for 2d plots

--- experiments/generate_data.py
+++ experiments/generate_data.py
@@ -21,12 +21,16 @@ medcov = D * medcorr * D
 hicorr = np.array([[1, 0.9], [0.9, 1]])
 hicov = D * hicorr * D
 
-tables = [("low", 2)]#, ("med", 2), ("hi", 2)]
+tables = [("low", 2), ("med", 2), ("hi", 2)]
 
 for tbl,dims in tables:
     # generate synthetic data
     if tbl == "low":
         cov = lowcov
+    elif tbl == "med":
+        cov = medcov
+    elif tbl == "hi":
+        cov = hicov
     if dims == 2:
         mean = mean2d
     #elif dims == 3:
@@ -54,10 +58,23 @@ for tbl,dims in tables:
     elif dims == 8:
         create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER, f INTEGER, h INTEGER);");
     create_qs.append("COPY %s FROM '%s.csv' WITH (DELIMITER ',');" % (name,name));
-create_qs.append("\histogram %s\n" % name)
+    create_qs.append("\histogram %s\n" % name)
+
+    ## Don't make histograms for these
+for tbl,dims in tables:
+    name = tbl + str(dims) + "_nohist"
+    create_qs.append("CREATE TABLE %s " % name);
+    if dims == 2:
+        create_qs.append("(x INTEGER, y INTEGER);");
+    elif dims == 3:
+        create_qs.append("(x INTEGER, y INTEGER, z INTEGER);");
+    elif dims == 8:
+        create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER, f INTEGER, h INTEGER);");
+    create_qs.append("COPY %s FROM '%s.csv' WITH (DELIMITER ',');" % (name,tbl+str(dims)));
+
 create =" ".join(create_qs)
 print create
 p = Popen(["../build/quickstep_cli_shell", "--initialize_db=true"], stdin=PIPE)
 
 out,err = p.communicate(input=create)
-print out
+#print out
--- experiments/measure.py
+++ experiments/measure.py
@@ -11,16 +11,16 @@ DEVNULL = open(os.devnull, 'wb')
 truesel = re.compile("\d+(?= rows)")
 estsel = re.compile("(?<=Selectivity = )[0|1].\d+")
 
-
+tbl="hi2"
 # execute quickstep queries
 ranges = range(0,1000,50)
-queries  = [ "SELECT * FROM low2 where x > %d;" % r for r in ranges]
+queries  = [ "SELECT * FROM %s where x > %d;" % (tbl,r) for r in ranges]
 est_results = [] 
 true_results = [] 
 for query in queries:
     p = Popen(["../build/quickstep_cli_shell", "--visualize_plan=true"], stdin=PIPE, stdout=DEVNULL, stderr=PIPE)
     _,res = p.communicate(input=query)
-    # parse output filei
+    # parse output file
     
     true = float(truesel.findall(res)[0])/500000
     est = estsel.findall(res)[1] ## the second one is selection op?
@@ -30,14 +30,37 @@ for query in queries:
     print "est",est
     print "true",true
 
+# execute queries on baseline 
+tbl = tbl + "_nohist"
+queries  = [ "SELECT * FROM %s where x > %d;" % (tbl,r) for r in ranges]
+trivest_results = [] 
+for query in queries:
+    p = Popen(["../build/quickstep_cli_shell", "--visualize_plan=true"], stdin=PIPE, stdout=DEVNULL, stderr=PIPE)
+    _,res = p.communicate(input=query)
+    # parse output file
+    
+    true = float(truesel.findall(res)[0])/500000
+    est = estsel.findall(res)[1] ## the second one is selection op?
+    est = float(est)
+    trivest_results.append(est)
+
 true_results = np.array(true_results)
 est_results= np.array(est_results)
+trivest_results= np.array(trivest_results)
 print (true_results - est_results)
-# plot error
-plt.plot(ranges, true_results - est_results, 'bo')
-plt.show()
-# overall error
-error = np.sum(np.abs(true_results - est_results))/np.sum(true_results)
 
+# plot error
+#plt.plot(ranges, trivtrue_results - trivest_results, 'ro', label='simple cost model')
+plt.plot(ranges, true_results - est_results, 'b.', label='histogram')
+plt.xlabel("A value")
+plt.ylabel("True selectivity - estimated selectivity")
+#plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# overall error
+hist_error = np.sum(np.abs(true_results - est_results))/np.sum(true_results)
+cm_error = np.sum(np.abs(true_results - trivest_results))/np.sum(true_results)
+print "error: ", hist_error, cm_error
 # plot
 
