45e44c982fbd50ad29136a267d868272f9f8759c
Jennifer Bi <jb3495@columbia.edu>
2019-05-09 14:20:12 -0400

    improved testing workflow

--- experiments/generate_data.py
+++ experiments/generate_data.py
@@ -5,41 +5,36 @@ import matplotlib.pyplot as plt
 import sys
 # standard deviations
 # range of data is [0, 1000) 
-mean2d = [500, 500]
-stddev2d = [340, 340]
-D = np.diag(stddev2d)
-
-# low correlation
-lowcorr = np.array([[1, 0], [0, 1]])
-lowcov = D * lowcorr * D
-
-# medium correlation
-medcorr = np.array([[1, 0.5], [0.5, 1]])
-medcov = D * medcorr * D
-
-# high correlation
-hicorr = np.array([[1, 0.9], [0.9, 1]])
-hicov = D * hicorr * D
-
-tables = [("low", 2), ("med", 2), ("hi", 2)]
-
+tables = [("low", 2), ("med", 2), ("hi", 2), ("low",3), ("med",3), ("hi", 3),
+            ("low", 5), ("med", 5), ("hi", 5)]
+#tables = [("unif", 2), ("unif", 3), ("unif", 5)]
 for tbl,dims in tables:
+    D = np.diag([340]*dims)
+    corr = np.ones((dims,dims),dtype=float)
     # generate synthetic data
     if tbl == "low":
-        cov = lowcov
+        corr = 0.1*corr
     elif tbl == "med":
-        cov = medcov
+        corr = 0.5*corr
     elif tbl == "hi":
-        cov = hicov
-    if dims == 2:
-        mean = mean2d
-    #elif dims == 3:
-    #    data = np.random.multivariate_normal(mean3d, lowcov, size=500000)
-    #elif dims == 8:
-    #    data = np.random.multivariate_normal(mean8d, lowcov, size=500000)
+        corr = 0.9*corr
+    np.fill_diagonal(corr,1)
+    cov = D * corr * D
+    mean = [0]*dims
     data = np.random.multivariate_normal(mean, cov, size=500000)
+    #data = np.random.uniform([-1000]*dims,[1000]*dims, size=(500000,dims))
+    """
+    arr = np.array([])
+    data = np.random.zipf(2,size=(63500,dims)) # produces around 500000
+    for d in range(0,dims):
+        vals = np.random.uniform(-1000,1000, 63500)
+        vals2 = np.random.uniform(-1000,1000, 63500)
+        freq = data[:,d]
+        for v,v2,f in zip(vals,vals2,freq):
+    """        
+    
     # uncomment to visualize one col 
-    #plt.hist(data[:,1], 100)
+    #plt.hist(data[:,0], 100)
     #plt.show()
     #sys.exit()
 
@@ -55,26 +50,30 @@ for tbl,dims in tables:
         create_qs.append("(x INTEGER, y INTEGER);");
     elif dims == 3:
         create_qs.append("(x INTEGER, y INTEGER, z INTEGER);");
-    elif dims == 8:
-        create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER, f INTEGER, h INTEGER);");
+    elif dims == 5:
+        create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER);");
     create_qs.append("COPY %s FROM '%s.csv' WITH (DELIMITER ',');" % (name,name));
     create_qs.append("\histogram %s\n" % name)
 
     ## Don't make histograms for these
+
 for tbl,dims in tables:
+    if dims == 5: 
+        continue
     name = tbl + str(dims) + "_nohist"
     create_qs.append("CREATE TABLE %s " % name);
     if dims == 2:
         create_qs.append("(x INTEGER, y INTEGER);");
     elif dims == 3:
         create_qs.append("(x INTEGER, y INTEGER, z INTEGER);");
-    elif dims == 8:
-        create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER, f INTEGER, h INTEGER);");
+    #elif dims == 5:
+    #    create_qs.append("(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER);");
     create_qs.append("COPY %s FROM '%s.csv' WITH (DELIMITER ',');" % (name,tbl+str(dims)));
 
 create =" ".join(create_qs)
 print create
-p = Popen(["../build/quickstep_cli_shell", "--initialize_db=true"], stdin=PIPE)
+#p = Popen(["../build/quickstep_cli_shell", "--initialize_db=true"], stdin=PIPE)
+p = Popen(["../build/quickstep_cli_shell"], stdin=PIPE)
 
 out,err = p.communicate(input=create)
 #print out
--- experiments/measure.py
+++ experiments/measure.py
@@ -3,54 +3,119 @@ from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
 import re
 import os
+import sys
+import math
 import numpy as np
+
+def execute_queries(tbl, dims):
+# execute quickstep queries
+    qtype = 1 # do gaussians first
+    c_v = 0.01 # 1 percent of data range
+    num_queries = 20
+    low = -1000
+    high = 1000
+    ranges = [] 
+    queries = []
+    step = math.pow(math.pow((high - low),dims)*c_v , 1.0/dims)
+    step = int(step/2.0)
+    print "%s: step: %d" %(tbl, step)
+
+    x,y,z,a,b,c,d,e=0,0,0,0,0,0,0,0
+
+    if qtype == 1: # GAUSS
+        ranges = np.random.multivariate_normal([0]*dims, 34*34*np.identity(dims), size=num_queries)
+    elif qtype == 2: # UNIFORM
+        ranges = np.random.uniform([low]*dims, [high]*dims, size=num_queries)
+
+    if dims == 2:
+        queries  = [ """SELECT * FROM %s where x > %d and x < %d
+                        and y > %d and y < %d;"""
+                    % (tbl,x-step, x+step, y-step, y+step) for (x,y) in ranges]
+    elif dims == 3:
+        queries  = [ """SELECT * FROM %s where x > %d and x < %d
+                        and y > %d and y < %d and z > %d and z < %d;"""
+                    % (tbl,x-step, x+step, y-step, y+step, z-step, z+step) for (x,y,z) in ranges]
+    elif dims == 5:
+        queries  = [ """SELECT * FROM %s where a > %d and a < %d
+                        and b > %d and b < %d and c > %d and c < %d
+                        and d > %d and d < %d and e > %d and e < %d;"""
+                    % (tbl,a-step,a+step,b-step,b+step,c-step,c+step,
+                        d-step,d+step,e-step,e+step) for (a,b,c,d,e) in ranges]
+    #print queries
+    est_results = [] 
+    true_results = [] 
+    for query in queries:
+        p = Popen(["../build/quickstep_cli_shell", "--visualize_plan=true"], stdin=PIPE, stdout=DEVNULL, stderr=PIPE)
+        _,res = p.communicate(input=query)
+        # parse output file
+        try:
+            l = truesel.findall(res.splitlines()[-1])
+            true = float(l[0])
+        except:
+            print res.splitlines()[-1]
+            print l
+            sys.exit()
+        est = estsel.findall(res)[1] ## the second one is selection op?
+        est = float(est)*500000
+        est_results.append(est)
+        true_results.append(true)
+        print "est",est
+        print "true",true
+
+    true_results = np.array(true_results)
+    est_results= np.array(est_results)
+    print "true_results",true_results
+    #print "est_results",est_results
+    # overall error
+    #error = np.sum(np.abs(true_results - est_results))/np.sum(true_results)
+    print(len(queries))
+    error = 100*np.sum(np.divide(np.abs(true_results - est_results),true_results))/len(queries)
+    print tbl, " error: ", error
+
+    return error 
+
+def generate_bar():
+    barwidth = 0.25
+    x = ["Gauss2d", "Gauss3d", "Unif"]
+    x_pos = [i for i, _ in enumerate(x)]
+    x_pos2 = [i + barwidth for i in x_pos]
+    x_pos3 = [i + barwidth for i in x_pos2]
+
+    errA = execute_queries("low2", 2);
+    errB = execute_queries("low3", 3);
+    errs = [errA, errB]#, errD]
+    plt.bar(x_pos, errs, color = '#1c7f27', edgecolor='white', width = barwidth, label='cov=0')
+
+    errA = execute_queries("med2", 2);
+    errB = execute_queries("med3", 3);
+    errs = [errA, errB]
+    plt.bar(x_pos2, errs, color = '#1459c9', edgecolor='white', width = barwidth, label='cov=0.5')
+
+    errA = execute_queries("hi2", 2);
+    errB = execute_queries("hi3", 3);
+    errs = [errA, errB]
+    plt.bar(x_pos3, errs, color = '#d8b62f', edgecolor='white', width = barwidth, label='cov=0.9')
+
+    plt.ylabel("Average error")
+
+    plt.xticks([i + barwidth for i in range(len(x_pos))], x)
+
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
 
 DEVNULL = open(os.devnull, 'wb')
 
 # regex for est and true selectivities
-truesel = re.compile("\d+(?= rows)")
+truesel = re.compile("\d+")
+#truesel = re.compile("\d+((?= rows)|(?= row))")
 estsel = re.compile("(?<=Selectivity = )[0|1].\d+")
 
-tbl="hi2"
-# execute quickstep queries
-ranges = range(0,1000,50)
-queries  = [ "SELECT * FROM %s where x > %d;" % (tbl,r) for r in ranges]
-est_results = [] 
-true_results = [] 
-for query in queries:
-    p = Popen(["../build/quickstep_cli_shell", "--visualize_plan=true"], stdin=PIPE, stdout=DEVNULL, stderr=PIPE)
-    _,res = p.communicate(input=query)
-    # parse output file
-    
-    true = float(truesel.findall(res)[0])/500000
-    est = estsel.findall(res)[1] ## the second one is selection op?
-    est = float(est)
-    est_results.append(est)
-    true_results.append(true)
-    print "est",est
-    print "true",true
+generate_bar()
+"""
 
-# execute queries on baseline 
-tbl = tbl + "_nohist"
-queries  = [ "SELECT * FROM %s where x > %d;" % (tbl,r) for r in ranges]
-trivest_results = [] 
-for query in queries:
-    p = Popen(["../build/quickstep_cli_shell", "--visualize_plan=true"], stdin=PIPE, stdout=DEVNULL, stderr=PIPE)
-    _,res = p.communicate(input=query)
-    # parse output file
-    
-    true = float(truesel.findall(res)[0])/500000
-    est = estsel.findall(res)[1] ## the second one is selection op?
-    est = float(est)
-    trivest_results.append(est)
-
-true_results = np.array(true_results)
-est_results= np.array(est_results)
-trivest_results= np.array(trivest_results)
-print (true_results - est_results)
 
 # plot error
-#plt.plot(ranges, trivtrue_results - trivest_results, 'ro', label='simple cost model')
 plt.plot(ranges, true_results - est_results, 'b.', label='histogram')
 plt.xlabel("A value")
 plt.ylabel("True selectivity - estimated selectivity")
@@ -58,9 +123,4 @@ plt.ylabel("True selectivity - estimated selectivity")
 plt.tight_layout()
 plt.show()
 
-# overall error
-hist_error = np.sum(np.abs(true_results - est_results))/np.sum(true_results)
-cm_error = np.sum(np.abs(true_results - trivest_results))/np.sum(true_results)
-print "error: ", hist_error, cm_error
-# plot
-
+"""
